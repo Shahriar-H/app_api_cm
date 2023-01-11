f7a2d267e5a8d9ff9eb5177b2c555ca6bb887b82
Shahriar-H <67462321+Shahriar-H@users.noreply.github.com>
2023-01-11 10:09:50 -0800

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,6 @@ const db = mysql.createConnection({
     user:process.env.DATABASE_USER,
     password:process.env.DATABASE_PASSWORD,
     database:process.env.DATABASE_NAME,
-    timeout: 60000
 })
 
 db.connect((error)=>{
