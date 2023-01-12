beb101df95546c73f7b14e38434a75ad2fdbaeec
Shahriar-H <67462321+Shahriar-H@users.noreply.github.com>
2023-01-12 08:00:56 -0800

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv')
 dotenv.config({path: './.env'})
 
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host:process.env.DATABASE_HOST,
     user:process.env.DATABASE_USER,
     password:process.env.DATABASE_PASSWORD,
